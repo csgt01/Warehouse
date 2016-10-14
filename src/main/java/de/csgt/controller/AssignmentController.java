abbfd0689a51c7da3c5a25fa7d50428d69a57144
cschulte <c.schulte@geomobile.de>
2016-10-14 14:12:07 +0200

    orders

---
+++ src/main/java/de/csgt/controller/AssignmentController.java
@@ -0,0 +1,60 @@
+package de.csgt.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import de.csgt.domain.Assignment;
+import de.csgt.service.AssignmentServiceInterface;
+import de.csgt.service.ShopServiceInterface;
+
+@Controller
+public class AssignmentController {
+	
+	@Autowired
+	private AssignmentServiceInterface assignmentService;
+	
+	@Autowired
+	private ShopServiceInterface shopService;
+	
+    @RequestMapping(value = "/assignments", method = RequestMethod.GET)
+    public String list(Model model){
+        model.addAttribute("assignments", assignmentService.listAllAssignments());
+        return "assignments";
+    }
+
+	@RequestMapping("assignment/new")
+	public String newAssignment(Model model) {
+		model.addAttribute("assignment", new Assignment());
+		model.addAttribute("shops", shopService.listAllShops());
+		return "assignmentform";
+	}
+
+	@RequestMapping(value = "assignment", method = RequestMethod.POST)
+	public String saveAssignment(Assignment assignment) {
+		assignmentService.saveAssignment(assignment);
+		return "redirect:/assignment/" + assignment.getId();
+	}
+	
+    @RequestMapping("assignment/{id}")
+    public String showAssignment(@PathVariable Long id, Model model){
+        model.addAttribute("assignment", assignmentService.getAssignmentById(id));
+        return "assignmentshow";
+    }
+
+	@RequestMapping("assignment/edit/{id}")
+	public String edit(@PathVariable Long id, Model model) {
+		model.addAttribute("assignment", assignmentService.getAssignmentById(id));
+		model.addAttribute("shops", shopService.listAllShops());
+		return "assignmentform";
+	}
+
+	@RequestMapping("assignment/delete/{id}")
+	public String delete(@PathVariable Long id) {
+		assignmentService.deleteAssignment(id);
+		return "redirect:/assignments";
+	}
+}
