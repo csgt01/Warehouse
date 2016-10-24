2c77368a67297df9b03cb49685beb135ecd5c27e
cschulte <c.schulte@geomobile.de>
2016-10-24 07:29:36 +0200

    new colors

--- src/main/java/de/csgt/domain/Color.java
+++ src/main/java/de/csgt/domain/Color.java
@@ -2,7 +2,10 @@ package de.csgt.domain;
 
 public enum Color {
 
-	BLUE("Blau"), RED("Rot"), WHITE("Weiß"), GREEN("Grün");
+	LIGHT_TURQUOISE("Helltürkis"), PINK2("Pink"), BLUE_PURPLE("Blau-Lila"), DARK_PINK("Dunkelpink"), 
+	PINK("Rosa"), TURQUOISE("Türkis"), LEMON("Lemon"), PURPLE("Flieder"), MIDDLE_BLUE("Mittelblau"), 
+	LIGHT_BLUE("BHellblau"), SHY_BLUE("Shyblau"), BLUE("Blau"), BABY_BLUE("Bybyblau"), 
+	RED("Rot"), WHITE("Weiß"), GREEN("Grün");
 	
 	private Color(String name) {
 		this.name = name;
