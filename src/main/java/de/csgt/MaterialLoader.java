08b867e2e68ae6e7f05a8768c3117fd0cea6fb84
cschulte <c.schulte@geomobile.de>
2016-11-04 08:18:38 +0100

    new loader

--- src/main/java/de/csgt/MaterialLoader.java
+++ src/main/java/de/csgt/MaterialLoader.java
@@ -1,13 +1,18 @@
 package de.csgt;
 
+import java.sql.Date;
+import java.util.Calendar;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import de.csgt.domain.Assignment;
 import de.csgt.domain.Color;
 import de.csgt.domain.Material;
 import de.csgt.domain.Shop;
+import de.csgt.service.AssignmentService;
 import de.csgt.service.MaterialService;
 import de.csgt.service.ShopService;
 
@@ -20,22 +25,403 @@ public class MaterialLoader implements ApplicationListener<ContextRefreshedEvent
 	@Autowired
     private ShopService shopService;
 
+	@Autowired
+	private AssignmentService assService;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
     	
-    	Shop shop = new Shop();
-    	shop.setId(1L);
-    	shop.setName("Murmelkiste");
+    	setupMaterials();
+    	Shop murmelkiste = new Shop();
+    	murmelkiste.setId(1L);
+    	murmelkiste.setName("Murmelkiste");
+    	murmelkiste.setCity("Berlin");
+    	shopService.saveShop(murmelkiste);
     	
-    	int i = 1;
-    	Material mat = new Material(i, "Motivperle", "", Color.BLUE, 10.0, 0);
-    	materialService.saveMaterial(mat);
-    	i++;
+    	Shop emmalein = new Shop();
+    	emmalein.setId(2L);
+    	emmalein.setName("emmalein");
+    	shopService.saveShop(emmalein);
     	
-    	mat = new Material(i, "Motivperle", "", Color.BABY_BLUE, 10.0, 0);
-    	materialService.saveMaterial(mat);
-    	i++;
+    	Assignment ass1 = new Assignment();
+    	ass1.setId(1L);
+    	ass1.setShop(murmelkiste);
+    	Calendar cal = Calendar.getInstance();
+    	cal.set(Calendar.YEAR, 2016);
+    	cal.set(Calendar.MONTH, 0);
+    	cal.set(Calendar.DAY_OF_MONTH, 26);
+    	ass1.setOrderedAt(new Date(cal.getTimeInMillis()));
+    	ass1.setOrderNumber("2016-0766");
+    	assService.saveAssignment(ass1);
     	
     }
+
+
+	private void setupMaterials() {
+		int i = 1;
+		Material mat = new Material(i, "Holzclip", "", Color.SEVERAL, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzperle", "", Color.BABY_BLUE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzperle", "", Color.PURPLE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzperle", "", Color.LEMON, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzperle", "", Color.TURQUOISE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzperle", "", Color.PINK, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzperle", "", Color.MIDDLE_BLUE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzperle", "", Color.DARK_PINK, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzperle", "", Color.WHITE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzperle", "", Color.BLUE_PURPLE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzperle", "", Color.LIGHT_TURQUOISE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzperle", "", Color.PINK2, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzbuchstabe A-Z", "", Color.WOOD, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzlinse", "", Color.SEVERAL, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Würfel", "", Color.PINK, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Würfel", "", Color.BABY_BLUE, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Sicherheitsperle", "", Color.WHITE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Cupcake", "", Color.SKY_BLUE, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Cupcake", "", Color.DARK_PINK, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Eule", "", Color.PINK, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Eule", "", Color.BABY_BLUE, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivscheibe Skull", "", Color.DARK_PINK, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Apfel", "", Color.SEVERAL, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivscheibe BORN 2015", "", Color.BABY_BLUE, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivscheibe BORN 2015", "", Color.PINK, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivscheibe Love Mom Dad", "", Color.BABY_BLUE, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivscheibe Love Mom Dad", "", Color.WHITE, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Linsen Sternchen", "", Color.BABY_BLUE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Linsen Sternchen", "", Color.WHITE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Linsen Sternchen", "", Color.PINK, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Pilz", "", Color.BABY_BLUE, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzperle", "", Color.PINK, 8.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzperle", "", Color.WHITE, 8.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzperle", "", Color.BABY_BLUE, 8.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Blümchen", "", Color.SEVERAL, 8.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Stern", "", Color.SEVERAL, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Glöckchen", "", Color.WHITE, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Glöckchen", "", Color.PINK, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Glöckchen", "", Color.LIGHT_BLUE, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzlinse", "", Color.BLUE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzlinse", "", Color.PINK, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzlinse", "", Color.SEVERAL, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Halbring", "", Color.SEVERAL, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Glitzerblümchen", "", Color.SEVERAL, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzring Mini", "", Color.WHITE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzring Mini", "", Color.MIDDLE_BLUE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Greifling Gr S", "", Color.PINK, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Herz Schnörkel", "", Color.SEVERAL, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Herz Glitzerpunkte", "", Color.PINK, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Herz Glitzerpunkte", "", Color.BABY_BLUE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Krönchen", "", Color.SEVERAL, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Schmetterling", "", Color.PINK, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Schmetterling", "", Color.PINK2, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Igel", "", Color.BABY_BLUE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Pferd", "", Color.PURPLE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Pferd", "", Color.PINK, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzlinse", "", Color.WHITE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzbuchstabe T", "", Color.WOOD, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Blume", "", Color.PINK, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Blume", "", Color.BABY_PINK, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Blume", "", Color.SILVER, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzring Mini", "", Color.GREY, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzbuchstabe A-Z", "", Color.WHITE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzbuchstabe Z", "", Color.WOOD, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Herz Schnörkel", "", Color.GREY, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzlinse", "", Color.LIGHT_GREY, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Halbring", "", Color.PURPLE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Halbring", "", Color.GREY, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Sicherheitsperle", "", Color.PURPLE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Sicherheitsperle", "", Color.PINK, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Sicherheitsperle", "", Color.BABY_PINK, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Sicherheitsperle", "", Color.DARK_PINK, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Sicherheitsperle", "", Color.PINK2, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Schmetterling", "", Color.BLUE_PURPLE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Schmetterling", "", Color.DARK_PINK, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Sicherheitsperle", "", Color.GREY, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzperle", "", Color.YELLOW_GREEN, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzperle", "", Color.SKY_BLUE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzperle", "", Color.BABY_PINK, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzperle", "", Color.GREY, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Herz Punkt", "", Color.PURPLE, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Herz Punkt", "", Color.DARK_PINK, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzring Mini", "", Color.SEVERAL, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Herz Schnörkel", "", Color.DARK_PINK, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Eule", "", Color.DARK_PINK, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Eule", "", Color.PURPLE, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Herz Schnörkel", "", Color.PURPLE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Herz Glitzerpunkte", "", Color.WHITE, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzperle", "", Color.LIGHT_GREY, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Holzclip", "", Color.LIGHT_GREY, 0.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+		mat = new Material(i, "Motivperle Herz Glitzerpunkte", "", Color.GREY, 10.0, 0);
+		materialService.saveMaterial(mat);
+		i++;
+		
+	}
 }
--- src/main/java/de/csgt/domain/Color.java
+++ src/main/java/de/csgt/domain/Color.java
@@ -2,10 +2,29 @@ package de.csgt.domain;
 
 public enum Color {
 
-	SEVERAL("Verschiedene Farben"), LIGHT_TURQUOISE("Helltürkis"), PINK2("Pink"), BLUE_PURPLE("Blau-Lila"), DARK_PINK("Dunkelpink"), 
-	PINK("Rosa"), TURQUOISE("Türkis"), LEMON("Lemon"), PURPLE("Flieder"), MIDDLE_BLUE("Mittelblau"), 
-	LIGHT_BLUE("BHellblau"), SHY_BLUE("Shyblau"), BLUE("Blau"), BABY_BLUE("Bybyblau"), 
-	RED("Rot"), WHITE("Weiß"), GREEN("Grün");
+	SEVERAL("Verschiedene Farben"), 
+	LIGHT_TURQUOISE("Helltürkis"), 
+	PINK2("Pink"), 
+	BLUE_PURPLE("Blau-Lila"), 
+	DARK_PINK("Dunkelpink"), 
+	PINK("Rosa"), 
+	TURQUOISE("Türkis"), 
+	LEMON("Lemon"), 
+	PURPLE("Flieder"), 
+	MIDDLE_BLUE("Mittelblau"), 
+	LIGHT_BLUE("Hellblau"), 
+	YELLOW_GREEN("Gelb-grün"),
+	SKY_BLUE("Skyblau"), 
+	BLUE("Blau"), 
+	BABY_BLUE("Babyblau"), 
+	RED("Rot"), 
+	WHITE("Weiß"), 
+	GREEN("Grün"), 
+	BABY_PINK("Babyrosa"), 
+	WOOD("Holz"), 
+	GREY("Grau"), 
+	LIGHT_GREY("Hellgrau"), 
+	SILVER("Silber");
 	
 	private Color(String name) {
 		this.name = name;
